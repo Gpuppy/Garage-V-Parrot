3f9855f9c0ac6c973839ad5e6c2076456f503764
Gael Adam <gaelswim@hotmail.com>
2024-02-09 11:40:29 +0100

    public added to Procfile, update webpack.config.js, update base.html

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,11 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+require('../css/app.scss');
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
+    .copyFiles({
+        from: './assets/images'
+    })
 
     /*
      * ENTRY CONFIG
