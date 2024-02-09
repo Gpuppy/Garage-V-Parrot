253bd354553cab5cc091953003446895abfc1ae0
Gael Adam <gaelswim@hotmail.com>
2024-02-09 14:35:28 +0100

    ? added in front of string in setImages SecondHandCar.php

--- src/Entity/SecondHandCar.php
+++ src/Entity/SecondHandCar.php
@@ -57,7 +57,7 @@ class SecondHandCar
         return $this->image;
     }
 
-    public function setImage(string $image ): self
+    public function setImage(?string $image ): self
     {
         $this->image = $image;
 
