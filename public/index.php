453d55f26a8f782ab96195835ce4ce40f48c16bf
Gael Adam <gaelswim@hotmail.com>
2024-02-17 18:00:12 +0100

    updated index.php, framework.yaml, doctrine.yaml and monolog.yaml

--- public/index.php
+++ public/index.php
@@ -3,15 +3,21 @@
 ##umask(0000); // This will let the permissions be 0777
 
 use App\Kernel;
+use http\Env\Request;
+
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 
+    if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+    }
 
 };
 
 #$_SERVER['HTTP_X_FORWARDED_PROTO'] = $_SERVER['HTTP_CUSTOM_FORWARDED_PROTO'];
 
 //$response = $kernel->handle($request);
+
