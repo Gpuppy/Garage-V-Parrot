d6239844adadfce05ca803b416b7eb1356101520
Gael Adam <gaelswim@hotmail.com>
2024-02-21 09:47:04 +0100

    file upload test form + s3 bucket created

--- public/index.php
+++ public/index.php
@@ -3,15 +3,18 @@
 umask(0002); // This will let the permissions be 0775
 
 use App\Kernel;
-use http\Env\Request;
+
 
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require('vendor/autoload.php');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 
-    if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+
+
+    /*if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
         Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
     }
 
@@ -20,9 +23,38 @@ return function (array $context) {
     if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
     if($trustedProxies) {
         Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-    }
+    }*/
 
 };
+
+
+$s3 = new Aws\S3\S3Client([
+    'version'  => '2006-03-01',
+    'region'   => 'Europe (Stockholm)',
+]);
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+?>
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<h1>S3 upload example</h1>
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
+    try {
+        // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+        ?>
+        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+    <?php } catch(Exception $e) { ?>
+        <p>Upload error :(</p>
+    <?php } } ?>
+<h2>Upload a file</h2>
+<form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <input name="userfile" type="file"><input type="submit" value="Upload">
+</form>
+</body>
+</html>
 
 #$_SERVER['HTTP_X_FORWARDED_PROTO'] = $_SERVER['HTTP_CUSTOM_FORWARDED_PROTO'];
 
