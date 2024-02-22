e4ee524862116adbf32c7d7e057bf61e8dc9f5f7
Gael Adam <gaelswim@hotmail.com>
2024-02-22 11:25:12 +0100

    configured aws client services in flysystem.yaml

--- public/index.php
+++ public/index.php
@@ -11,18 +11,6 @@ return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 
 
-
-    /*if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-    }
-
-    $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-    $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-    if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-    if($trustedProxies) {
-        Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-    }*/
-
 };
 
 
