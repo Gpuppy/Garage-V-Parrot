d265eb0f7e03856b3b50c023a386d1f6cbc76300
Gael Adam <gaelswim@hotmail.com>
2024-03-12 15:58:59 +0100

    services.yaml updated secondhancar enabled

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ umask(0002); // This will let the permissions be 0775
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-require('vendor/autoload.php');
+/*require('vendor/autoload.php');*/
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
