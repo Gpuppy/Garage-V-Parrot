fd98422d2fa6210a21707fccfe7ae2ea1121e5cd
Gael Adam <gaelswim@hotmail.com>
2024-04-19 19:36:15 +0200

    review3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Home;
+
+use App\Entity\Review;
+use App\Form\ReviewType;
+//use http\Env\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,9 +21,31 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+
     }
-    public function show(Home $home) : Response
+
+    /*public function show(Request $request, EntityManagerInterface $manager) : Response
     {
-        return $this->render('home/index.html.twig');
-    }
+        $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review );
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $review = $form->getData();
+            //$reviewService->persistReview($review, null);
+
+            $manager->persist($review);
+            $manager->flush();
+
+
+            $this->addFlash('success', 'Votre avis nous a bien été envoyé merci! :) ');
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/index.html.twig',[
+            //'home' => $home,
+            'form' => $form->createView(),
+        ]);
+    }*/
 }
--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Review;
+use App\Form\ReviewType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReviewController extends AbstractController
 {
-    #[Route('/avis', name: 'app_review')]
-    public function index(): Response
+    #[Route('/review', name: 'review/index.html.twig')]
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
+        $review = new Review();
+        $form = $this->createForm(ReviewType::class, $review );
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $review = $form->getData();
+
+            $manager->persist($review);
+            $manager->flush();
+
+
+            $this->addFlash('success', 'Votre avis nous a bien été envoyé merci! :) ');
+
+            return $this->redirectToRoute('review/index.html.twig');
+        }
         return $this->render('review/index.html.twig', [
             'controller_name' => 'ReviewController',
+            'form' => $form->createView()
         ]);
     }
+
 }
